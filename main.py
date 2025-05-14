94fdda1576a404638114458694dbf5f98298ed96
sxkiss <admin@sxkiss.com>
2025-05-14 20:57:19 +0800

    Last Sync: 2025-05-14 12:57:19 (Mobile)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import json
+import re
+import tomllib
+import traceback
+import aiohttp
+import filetype
 from loguru import logger
-
+from typing import Union
 from WechatAPI import WechatAPIClient
+from database.XYBotDB import XYBotDB
 from utils.decorators import *
 from utils.plugin_base import PluginBase
-from database.XYBotDB import XYBotDB
-import aiohttp
-import tomllib
+import logging
 
 
 class Bncr(PluginBase):
@@ -31,80 +36,52 @@ class Bncr(PluginBase):
 
     async def request(self, message):
         try:
-            logger.debug(message)
-            async with aiohttp.request("POST", url=self.base_url, json=message) as req:
-                return await req.json()
+            logger.info("推送无界成功")
+            async with aiohttp.request("post", url=self.base_url, json=message) as req:
+                return await req.json()             
         except Exception as e:
-            logger.error(f"推送无界失败: {str(e)}")
+            logger.error(f"推送无界失败:{str(e)}")
+            logger.error(f"推送无界失败:{req.status} - {req.text()}")
             return
 
-    @on_text_message(priority=80)
+    @on_text_message
     async def handle_text(self, bot: WechatAPIClient, message: dict):
+        logger.info("收到了无界文本消息")
         return await self.request(message)
-        
+
     @on_at_message
     async def handle_at(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了被@消息")
+        logger.info("收到了无界被@消息")
         return await self.request(message)
-        
-    @on_xml_message
-    async def handle_xml(self, bot: WechatAPIClient, message: dict):
-       logger.info("收到了xml消息")
-       return await self.request(message)
-         
-    @on_friend_request(priority=99)
-    async def handle_friend(self, bot: WechatAPIClient, message: dict):
-       logger.info("收到了好友申请")
-       return await self.request(message)
-
-    @on_voice_message
-    async def handle_voice(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了语音消息")
-        #return await self.request(message)
 
     @on_image_message
     async def handle_image(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了图片消息")
-         #return await self.request(message)
+        logger.info("收到了无界图片消息")
+        return await self.request(message)
 
     @on_video_message
     async def handle_video(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了视频消息")
-         #return await self.request(message)
+        logger.info("收到了无界视频消息")
+        return await self.request(message)
 
     @on_file_message
     async def handle_file(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了文件消息")
+        logger.info("收到了无界文件消息")
         return await self.request(message)
 
     @on_quote_message
     async def handle_quote(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了引用消息")
-        #return await self.request(message)
-
-    @on_sys_message
-    async def handle_sys(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了系统消息")
-        #return await self.request(message)
+        logger.info("收到了无界引用消息")
+        return await self.request(message)
 
     @on_pat_message
     async def handle_pat(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了拍一拍消息")
+        logger.info("收到了无界拍一拍消息")
         return await self.request(message)
-
-    @on_emoji_message
-    async def handle_emoji(self, bot: WechatAPIClient, message: dict):
-        logger.info("收到了表情消息")
+    # @on_sys_message
+    # async def handle_sys(self, bot: WechatAPIClient, message: dict):
+        # logger.info("收到了无界系统消息")
         #return await self.request(message)
-
-    # @schedule('interval', seconds=5)
-    # async def periodic_task(self, bot: WechatAPIClient):
-    #     logger.info("我每5秒执行一次")
-
-    # @schedule('cron', hour=8, minute=30, second=30)
-    # async def daily_task(self, bot: WechatAPIClient):
-    #     logger.info("我每天早上8点30分30秒执行")
-
-    # @schedule('date', run_date='2025-01-29 00:00:00')
-    # async def new_year_task(self, bot: WechatAPIClient):
-    #     logger.info("我在2025年1月29日执行")
+    @schedule('interval', seconds=5)
+    async def periodic_task(self, bot: WechatAPIClient):
+        logger.info("每5秒无界执行一次")
